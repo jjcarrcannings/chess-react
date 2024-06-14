3b35682b62b06eed9bccfc4e4982ae2831546f26
jjcarrcannings <jjcarrcannings@gmail.com>
2024-06-14 14:25:46 -0700

    move logic

--- src/App.js
+++ src/App.js
@@ -33,48 +33,45 @@ function App() {
 
   let handleClick = pos => {
     const squareClicked = currentSquares[pos[0]][pos[1]];
-    if(pieceSelected.length==0){
-      if(squareClicked && isPieceWhite(squareClicked)==isWhiteTurn){
+    if(squareClicked && isPieceWhite(squareClicked)==isWhiteTurn){ // Click is on active player's colour
         setPieceSelected(pos);
         setAvailableDestinations(calculateAvailableDestinations(pos));
-        return;
-      }
-      else return;
-    } 
-    if(availableDestinations.some(e=>e.toString()==pos.toString())){ //Click corresponds to valid move
+    } else if(availableDestinations.some(e=>e.toString()==pos.toString())){ // Click corresponds to valid move destination
       executeMove(pieceSelected, pos);
       endTurn();
+    } else{ // Invalid click
+      setPieceSelected([]);
+      setAvailableDestinations([]);
     }
     return;
   };
 
-  let calculateAvailableDestinations = pos => {
-    const piece = currentSquares[pos[0]][pos[1]];
-    let destinations = pieceConfig[piece].getValidDestinations(pos,isPieceWhite(piece))
-                                          .filter((move)=>!isKingInCheckIfMove(move,isPieceWhite(piece)));
-    return destinations;
+  let calculateAvailableDestinations = (pos, squares=currentSquares) => {
+    const piece = squares[pos[0]][pos[1]];
+    return pieceConfig[piece].getValidDestinations(pos,isPieceWhite(piece));
+
   }
 
-  let getValidPawnDestinations = (pos, isWhite) => {
+  let getValidPawnDestinations = (pos, isWhite, squares=currentSquares) => {
     const direction = isWhite ? -1 : 1;
     const validMoves = [
                                 ...[[direction,-1],[direction,1]]
                                 .map((e,i) => e.map((x,j) => x + pos[j]))
                                 .filter((move) => isOnBoard(move)
-                                                  && isSquareOccupiedOppositeColour(move,isWhite)),
+                                                  && isSquareOccupiedOppositeColour(move,isWhite, squares)),
                                 ...[[direction,0].map((e,i) => e + pos[i])]
                                 .filter((move) => isOnBoard(move)
-                                                  && isSquareEmpty(move)),
+                                                  && isSquareEmpty(move, squares)),
                                 ...(((isWhite && pos[0]==6) || (!isWhite && pos[0]==1)) 
                                       ? [[direction*2,0].map((e,i) => e + pos[i])]
-                                        .filter((move) => isSquareEmpty([direction,0].map((e,i)=>e+pos[i]))
-                                                          && isSquareEmpty(move)) 
+                                        .filter((move) => isSquareEmpty([direction,0].map((e,i)=>e+pos[i]), squares)
+                                                          && isSquareEmpty(move, squares)) 
                                       : [])
                               ];
-    return validMoves;
+    return validMoves.filter((move) => !isKingInCheckIfMove(pos, move, isWhite, squares));
   }
 
-  let getValidRookDestinations = (pos, isWhite) => {
+  let getValidRookDestinations = (pos, isWhite, squares=currentSquares) => {  
     let validMoves = [];
     for(let moveDirection of [[-1,0],[0,-1],[1,0],[0,1]]){
       let currentMove = moveDirection.map((e,i)=>e+pos[i]);
@@ -90,25 +87,97 @@ function App() {
         currentMove = moveDirection.map((e,i)=>e+currentMove[i]);
       }
     }
-    return validMoves.filter((move) => !isKingInCheckIfMove(move, isWhite));
+    return validMoves.filter((move => !isKingInCheckIfMove(pos, move, isWhite, squares)));
   }
 
-  let isKingInCheckIfMove = (posTo, isWhite) => {
+  let getValidKnightDestinations = (pos, isWhite, squares=currentSquares) => {
+    const jumps = [[-2,-1],[-2,1],[-1,2],[1,2],[2,1],[2,-1],[1,-2],[-1,-2]];
+    let validMoves = jumps.map( (jump,i) => jump.map( (e,j) => e + pos[j] ) )
+                      .filter((move) => isOnBoard(move) && !isSquareOccupiedSameColour(move, isWhite)
+                                                        && !isKingInCheckIfMove(pos, move, isWhite, squares));
+    return validMoves;
+  }
+
+  let getValidBishopDestinations = (pos, isWhite, squares=currentSquares) => {
+    let validMoves = [];
+    for(let moveDirection of [[-1,-1],[1,-1],[1,1],[-1,1]]){
+      let currentMove = moveDirection.map((e,i)=>e+pos[i]);
+      while(isOnBoard(currentMove)){
+        if(isSquareEmpty(currentMove)){
+          validMoves.push(currentMove);
+        } else if(isSquareOccupiedSameColour(currentMove,isWhite)){ 
+          break;
+        } else{
+          validMoves.push(currentMove);
+          break;
+        }
+        currentMove = moveDirection.map((e,i)=>e+currentMove[i]);
+      }
+    }
+    return validMoves.filter((move => !isKingInCheckIfMove(pos, move, isWhite, squares)));
+  }
+
+  let getValidQueenDestinations = (pos, isWhite, squares=currentSquares) => {
+    let validMoves = [];
+    for(let moveDirection of [[-1,-1],[1,-1],[1,1],[-1,1],[-1,0],[0,-1],[1,0],[0,1]]){
+      let currentMove = moveDirection.map((e,i)=>e+pos[i]);
+      while(isOnBoard(currentMove)){
+        if(isSquareEmpty(currentMove)){
+          validMoves.push(currentMove);
+        } else if(isSquareOccupiedSameColour(currentMove,isWhite)){ 
+          break;
+        } else{
+          validMoves.push(currentMove);
+          break;
+        }
+        currentMove = moveDirection.map((e,i)=>e+currentMove[i]);
+      }
+    }
+    return validMoves.filter((move => !isKingInCheckIfMove(pos, move, isWhite, squares)));
+  }
+
+  let getValidKingDestinations = (pos, isWhite, squares=currentSquares) => {
+    const moves = [[-1,-1],[1,-1],[1,1],[-1,1],[-1,0],[0,-1],[1,0],[0,1]];
+    let validMoves = moves.map((move,i)=>move.map((e,j)=>e+pos[j]))
+                      .filter((move) => isOnBoard(move) && !isSquareOccupiedSameColour(move, isWhite)
+                                                        && !isKingInCheckIfMove(pos, move, isWhite, squares));
+    //TODO: Castling
+    return validMoves;
+  }
+
+  let getAllPieceDestinationsForColour = (isWhite) => {
+    let result = []
+    for(let i=0; i<8; i++){
+      for(let j=0; j<8; j++){
+        const piece = currentSquares[i][j];
+        if(piece && isPieceWhite(piece)==isWhite){
+          result.push(pieceConfig[piece].getValidDestinations([i,j],isWhite));
+        }
+      }
+    }
+    return result;
+  }
+
+  let isKingInCheckIfMove = (posFrom, posTo, isWhite, squares) => {
     // TODO: Add logic
     return false;
+  }
+
+  let isCheckByColour = (squares, isWhite) => {
+    
   }
 
   let isPieceWhite = (piece) => piece.charCodeAt(0) <= 0x2659;
 
   let isOnBoard = pos => pos[0] >= 0 && pos[0] < 8 && pos[1] >= 0 && pos[1] < 8 ;
 
-  let isSquareOccupiedOppositeColour = (pos, isWhite) => currentSquares[pos[0]][pos[1]]
-                                                          && isPieceWhite(currentSquares[pos[0]][pos[1]]) != isWhite;
+  let isSquareOccupiedOppositeColour = (pos, isWhite, squares=currentSquares) => squares[pos[0]][pos[1]]
+                                                          && isPieceWhite(squares[pos[0]][pos[1]]) != isWhite;
 
-  let isSquareOccupiedSameColour = (pos, isWhite) => currentSquares[pos[0]][pos[1]]
+  let isSquareOccupiedSameColour = (pos, isWhite, squares=currentSquares) => squares[pos[0]][pos[1]]
                                                       && isPieceWhite(currentSquares[pos[0]][pos[1]]) == isWhite;
            
-  let isSquareEmpty = (pos) => currentSquares[pos[0]][pos[1]] == null;
+  let isSquareEmpty = (pos, squares=currentSquares) => squares[pos[0]][pos[1]] == null;
 
   let executeMove = (posFrom, posTo) => {
     const movingPiece = currentSquares[posFrom[0]][posFrom[1]];
@@ -146,16 +215,16 @@ function App() {
       getValidDestinations: getValidRookDestinations,
     },
     [PIECES.WHITE_KNIGHT]: {
-
+      getValidDestinations: getValidKnightDestinations,
     },
     [PIECES.WHITE_BISHOP]: {
-
+      getValidDestinations: getValidBishopDestinations,
     },
     [PIECES.WHITE_QUEEN]: {
-
+      getValidDestinations: getValidQueenDestinations,
     },
     [PIECES.WHITE_KING]: {
-
+      getValidDestinations: getValidKingDestinations,
     },
     [PIECES.BLACK_PAWN]: {
       getValidDestinations: getValidPawnDestinations
@@ -164,16 +233,16 @@ function App() {
       getValidDestinations: getValidRookDestinations,
     },
     [PIECES.BLACK_KNIGHT]: {
-
+      getValidDestinations: getValidKnightDestinations,
     },
     [PIECES.BLACK_BISHOP]: {
-
+      getValidDestinations: getValidBishopDestinations,
     },
     [PIECES.BLACK_QUEEN]: {
-
+      getValidDestinations: getValidQueenDestinations,
     },
     [PIECES.BLACK_KING]: {
-
+      getValidDestinations: getValidKingDestinations,
     },
   };
 
